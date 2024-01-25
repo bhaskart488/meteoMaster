41e7c3ff82958d0d7d51e73892b91a0d02b68d44
Bhaskar Trivedi <bhaskartrivedi488@gmail.com>
2024-01-25 17:51:58 +0530

    add complete functionality

--- charts.py
+++ charts.py
@@ -25,3 +25,24 @@ def get_main_image():
     img.seek(0)
     return img
 
+
+def get_city_image(city_id):
+    """Rendering line charts with city specific data"""
+    city = data.get(city_id)
+    city_temp = get_city_temperature(city)
+    city_hum = get_city_humidity(city)
+
+    plt.clf()
+    plt.plot(MONTHS, city_temp, color='blue', linewidth=2.5, linestyle='-')
+    plt.ylabel('Mean Daily Temperature', color='blue')
+    plt.yticks(color='blue')
+    plt.twinx()
+    plt.plot(MONTHS, city_hum, color='red', linewidth=2.5, linestyle='-')
+    plt.ylabel('Average Relative Humidity', color='red')
+    plt.yticks(color='red')
+    plt.title(city.city_name)
+
+    img = BytesIO()
+    plt.savefig(img)
+    img.seek(0)
+    return img
